954d2ee1592564a935ec4e6f362e989a015d60d0
Keila Lopez <keilalofra@gmail.com>
2022-09-19 15:19:10 -0500

    commit tree aaf561670ab74565783a4523ca51dd629c8366bf

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+};
+
+function appendCat (name) {
+    let newCats = [...cats, name];
+    return newCats;
+};
+
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats
+};
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+};
+
+function removeFirstCat() {
+    return cats.slice(1);
+};
